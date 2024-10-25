61819358806c02bef2057dc42b00d36b823d5ebc
nyambogahezron <hezzyn1@gmail.com>
2024-10-25 20:29:23 +0300

    add tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,39 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import TabBar from '../components/TabBar.tsx'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+        tabBar={props=> <TabBar {...props} />}
+    >
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Home"
+            }}
+        />
+        <Tabs.Screen
+            name="explore"
+            options={{
+                title: "Explore"
+            }}
+        />
+        <Tabs.Screen
+            name="create"
+            options={{
+                title: "Create"
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: "Profile"
+            }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default _layout
