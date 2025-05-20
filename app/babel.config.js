5e882aee74ab8339b5cd9c6182746689f5fc0e9b
nyambogahezron <hezzyn1@gmail.com>
2025-05-20 18:50:00 +0300

    fix app

--- app/babel.config.js
+++ app/babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',
       'react-native-reanimated/plugin',
+      'expo-router/babel'
     ],
   };
 };
